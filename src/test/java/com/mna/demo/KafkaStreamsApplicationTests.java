c0c2db1e42178c43faa66aed814dd2919776b5fa
Mark Nuttall <Mark.Nuttall Mark.Nuttall@GSOLT-DQYCP12.gso.gilbarco.com>
2018-02-08 11:10:02 -0500

    Renamed package and test name

--- src/test/java/com/mna/demo/KafkaStreamsApplicationTests.java
+++ src/test/java/com/mna/demo/KafkaStreamsApplicationTests.java
@@ -1,4 +1,4 @@
-package com.gilbarco.passport;
+package com.mna.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class KafkaStreamsTry2ApplicationTests {
+public class KafkaStreamsApplicationTests {
 
 	@Test
 	public void contextLoads() {
